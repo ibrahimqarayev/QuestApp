17e00cdcfb2a7544c3801e8aff15a3b29c599307
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-04 15:38:11 +0400

    Post

--- src/main/java/com/quest/entity/Post.java
+++ src/main/java/com/quest/entity/Post.java
@@ -1,4 +1,20 @@
 package com.quest.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "post")
 public class Post {
+    @Id
+    private Long id;
+    private Long userId;
+    private String title;
+
+    @Lob
+    @Column(columnDefinition = "text")
+    private String text;
+
 }
