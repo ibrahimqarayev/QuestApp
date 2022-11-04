816c3ab0d70075d247d267ee21ae5cd6a683a1ae
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-04 15:37:58 +0400

    User

--- src/main/java/com/quest/entity/User.java
+++ src/main/java/com/quest/entity/User.java
@@ -1,15 +1,15 @@
 package com.quest.entity;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.Data;
+import javax.persistence.*;
 
 @Entity
+@Data
 @Table(name = "user")
 public class User {
 
     @Id
     private Long id;
+
     private String userName;
     private String password;
 }
