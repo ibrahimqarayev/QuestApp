ea0f3bf54c2e1588f0bb085019a6246e3265fda5
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-04 15:36:06 +0400

    Like

--- src/main/java/com/quest/entity/Like.java
+++ src/main/java/com/quest/entity/Like.java
@@ -1,4 +1,17 @@
 package com.quest.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "p_like")
+@Data
 public class Like {
+    @Id
+    private Long id;
+    private Long postId;
+    private Long userId;
 }
