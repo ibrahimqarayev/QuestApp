ce36f5c36e445d0b1554c14f1b6faf21a9d1ab61
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-04 15:35:14 +0400

    Comment

--- src/main/java/com/quest/entity/Comment.java
+++ src/main/java/com/quest/entity/Comment.java
@@ -1,4 +1,21 @@
 package com.quest.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "comment")
 public class Comment {
+    @Id
+    private Long id;
+
+    private Long postId;
+
+    private Long userId;
+
+    @Lob
+    @Column(columnDefinition = "text")
+    private String text;
 }
