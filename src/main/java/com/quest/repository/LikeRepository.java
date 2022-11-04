ad82fcb5ef02cdbc2d8dd57cffda104a82dc0969
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-04 15:40:52 +0400

    LikeRepository

---
+++ src/main/java/com/quest/repository/LikeRepository.java
@@ -0,0 +1,9 @@
+package com.quest.repository;
+import com.quest.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like,Long> {
+
+
+}
+
