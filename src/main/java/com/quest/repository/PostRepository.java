51d3e8b4753b0f39ad673c3ca64a014fa155dd03
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-05 17:23:54 +0400

    PostRepository

--- src/main/java/com/quest/repository/PostRepository.java
+++ src/main/java/com/quest/repository/PostRepository.java
@@ -1,7 +1,8 @@
 package com.quest.repository;
-
 import com.quest.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findByUserId(Long userId);
 }
