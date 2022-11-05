e8ca575f2988c77ee66aa9c3f15a14a8a72f70a0
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-05 16:31:24 +0400

    UserServiceImpl

---
+++ src/main/java/com/quest/service/impl/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package com.quest.service.impl;
+
+import com.quest.entity.User;
+import com.quest.repository.UserRepository;
+import com.quest.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User saveOneUser(User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @Override
+    public User getOneUser(Long userId) {
+        User user = userRepository.findById(userId).orElse(null);
+        return user;
+    }
+
+    @Override
+    public User updateOneUser(Long userId, User newUser) {
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            User foundUser = user.get();
+            foundUser.setUserName(newUser.getUserName());
+            foundUser.setPassword(newUser.getPassword());
+            return foundUser;
+        } else
+            return null;
+    }
+
+    @Override
+    public void deleteById(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
