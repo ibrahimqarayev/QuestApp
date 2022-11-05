af2e3d898b32d32c1a4d460ba52c2fb306099718
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-05 14:35:14 +0400

    Post

--- src/main/java/com/quest/entity/Post.java
+++ src/main/java/com/quest/entity/Post.java
@@ -1,6 +1,9 @@
 package com.quest.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
@@ -10,7 +13,13 @@ import javax.persistence.*;
 public class Post {
     @Id
     private Long id;
-    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    User user;
+
     private String title;
 
     @Lob
