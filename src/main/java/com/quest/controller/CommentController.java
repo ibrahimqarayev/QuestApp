f0a65f1f7b4cf039efb9dc12de8cd36958ca9dc7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-06 21:08:49 +0400

    CommentController

---
+++ src/main/java/com/quest/controller/CommentController.java
@@ -0,0 +1,20 @@
+package com.quest.controller;
+import com.quest.service.CommentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+
+
+
+
+}
