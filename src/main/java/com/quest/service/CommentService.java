9a75f95fce34509f38acaa179632179d3dfdc078
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-06 21:23:51 +0400

    CommentService

--- src/main/java/com/quest/service/CommentService.java
+++ src/main/java/com/quest/service/CommentService.java
@@ -1,6 +1,11 @@
 package com.quest.service;
+import com.quest.entity.Comment;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import java.util.Optional;
 
 public interface CommentService {
 
+    List<Comment> getCommentWithParam(@RequestParam Optional<Long> userId, @RequestParam Optional<Long> postId);
 
 }
