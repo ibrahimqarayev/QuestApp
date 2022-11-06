517142a5c548c78b402c46ab6b00e33e4f519d43
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-06 21:24:00 +0400

    CommentServiceImpl

--- src/main/java/com/quest/service/impl/CommentServiceImpl.java
+++ src/main/java/com/quest/service/impl/CommentServiceImpl.java
@@ -1,7 +1,10 @@
 package com.quest.service.impl;
+import com.quest.entity.Comment;
 import com.quest.repository.CommentRepository;
 import com.quest.service.CommentService;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CommentServiceImpl implements CommentService {
@@ -12,6 +15,17 @@ public class CommentServiceImpl implements CommentService {
         this.commentRepository = commentRepository;
     }
 
+    @Override
+    public List<Comment> getCommentWithParam(Optional<Long> userId, Optional<Long> postId) {
+        if (userId.isPresent() && postId.isPresent()) {
+            return commentRepository.findByUserIdAndPostId(userId.get(), postId.get());
+        } else if (userId.isPresent()) {
+            return commentRepository.findByUserId(userId.get());
+        } else if (postId.isPresent()) {
+            return commentRepository.findByPostId(postId.get());
+        } else
+            return commentRepository.findAll();
+    }
 
 
 }
