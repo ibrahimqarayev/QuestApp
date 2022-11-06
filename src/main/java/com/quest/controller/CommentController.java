d37de81a13a580a29d96f04e69c927bdbf594d99
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-06 21:23:38 +0400

    CommentRepository

--- src/main/java/com/quest/controller/CommentController.java
+++ src/main/java/com/quest/controller/CommentController.java
@@ -1,7 +1,14 @@
 package com.quest.controller;
+
+import com.quest.entity.Comment;
 import com.quest.service.CommentService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/comments")
@@ -12,8 +19,6 @@ public class CommentController {
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
     }
-
-
 
 
 
--- src/main/java/com/quest/repository/CommentRepository.java
+++ src/main/java/com/quest/repository/CommentRepository.java
@@ -1,9 +1,18 @@
 package com.quest.repository;
 import com.quest.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByUserIdAndPostId(Long userId, Long postId);
+
+
+    List<Comment> findByUserId(Long userId);
+
+    List<Comment> findByPostId(Long postId);
+
 
 
 
