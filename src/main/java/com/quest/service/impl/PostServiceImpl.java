b717dea56703339217c0adfd57b90a3a28898a6a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-06 20:10:24 +0400

    PostServiceImpl

--- src/main/java/com/quest/service/impl/PostServiceImpl.java
+++ src/main/java/com/quest/service/impl/PostServiceImpl.java
@@ -1,8 +1,11 @@
 package com.quest.service.impl;
 
 import com.quest.entity.Post;
+import com.quest.entity.User;
 import com.quest.repository.PostRepository;
+import com.quest.request.PostCreateRequest;
 import com.quest.service.PostService;
+import com.quest.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,10 +15,13 @@ import java.util.Optional;
 public class PostServiceImpl implements PostService {
 
     private PostRepository postRepository;
+    private UserService userService;
 
-    public PostServiceImpl(PostRepository postRepository) {
+    public PostServiceImpl(PostRepository postRepository, UserService userService) {
         this.postRepository = postRepository;
+        this.userService = userService;
     }
+
 
     @Override
     public List<Post> getAllPosts(Optional<Long> userId) {
@@ -27,6 +33,19 @@ public class PostServiceImpl implements PostService {
     @Override
     public Post getOnePostById(Long postId) {
         return postRepository.findById(postId).orElse(null);
+    }
+
+    @Override
+    public Post createOnePost(PostCreateRequest newPostRequest) {
+        User user = userService.getOneUser(newPostRequest.getUserId());
+        if (user == null)
+            return null;
+        Post toSave = new Post();
+        toSave.setId(newPostRequest.getId());
+        toSave.setTitle(newPostRequest.getTitle());
+        toSave.setText(newPostRequest.getText());
+        toSave.setUser(user);
+        return postRepository.save(toSave);
     }
 
 
