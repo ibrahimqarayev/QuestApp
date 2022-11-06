a9cba12c8db94880692676d70fbef9508ba2e101
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-06 22:29:00 +0400

    CommentService

--- src/main/java/com/quest/service/CommentService.java
+++ src/main/java/com/quest/service/CommentService.java
@@ -1,5 +1,7 @@
 package com.quest.service;
 import com.quest.entity.Comment;
+import com.quest.request.comment.CommentCreateRequest;
+import com.quest.request.comment.CommentUpdateRequest;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.Optional;
@@ -8,4 +10,11 @@ public interface CommentService {
 
     List<Comment> getCommentWithParam(@RequestParam Optional<Long> userId, @RequestParam Optional<Long> postId);
 
+    Comment getOneCommentById(Long commentId);
+
+    Comment createOneComment(CommentCreateRequest createComment);
+
+    Comment updateOneComment(Long commentId, CommentUpdateRequest updateRequest);
+
+    void deleteOneCommentById(Long commentId);
 }
