680e65ff3bcd4234e9bb3050cd761efd37e4f2d6
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-07 22:01:57 +0400

    LikeRepository

--- src/main/java/com/quest/repository/LikeRepository.java
+++ src/main/java/com/quest/repository/LikeRepository.java
@@ -1,9 +1,19 @@
 package com.quest.repository;
+
 import com.quest.entity.Like;
+import com.quest.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikeRepository extends JpaRepository<Like,Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
 
 
+    List<Like> findByPostIdAndUserId(Optional<Long> postId, Optional<Long> userId);
+
+    List<Like> findByPostId(Long aLong);
+
+    List<Like> findByUserId(Long aLong);
 }
 
