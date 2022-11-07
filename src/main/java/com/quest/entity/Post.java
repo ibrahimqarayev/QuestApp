4dab1000899354aab557d95b81ecdf5b00047c25
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-07 22:03:09 +0400

    Post

--- src/main/java/com/quest/entity/Post.java
+++ src/main/java/com/quest/entity/Post.java
@@ -14,10 +14,9 @@ public class Post {
     @Id
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
-    @JsonIgnore
     User user;
 
     private String title;
