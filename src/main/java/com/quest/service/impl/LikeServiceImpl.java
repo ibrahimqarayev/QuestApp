b8a0a46829554e36abc6815f57f70af48c5db266
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-07 18:15:48 +0400

    LikeServiceImpl

---
+++ src/main/java/com/quest/service/impl/LikeServiceImpl.java
@@ -0,0 +1,7 @@
+package com.quest.service.impl;
+import com.quest.service.LikeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LikeServiceImpl implements LikeService {
+}
