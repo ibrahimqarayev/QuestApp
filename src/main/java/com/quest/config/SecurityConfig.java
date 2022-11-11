92f8fe1d0de66c824cc773dd29f5e1bed91cba57
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-11 23:55:50 +0400

    SecurityConfig

--- src/main/java/com/quest/config/SecurityConfig.java
+++ src/main/java/com/quest/config/SecurityConfig.java
@@ -1,7 +1,17 @@
 package com.quest.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
