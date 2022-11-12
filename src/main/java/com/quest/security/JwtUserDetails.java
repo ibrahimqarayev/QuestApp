59797c56182bdb9b08f75a510575615f00dc9274
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-11-12 22:40:05 +0400

    JwtUserDetails

--- src/main/java/com/quest/security/JwtUserDetails.java
+++ src/main/java/com/quest/security/JwtUserDetails.java
@@ -1,26 +1,21 @@
 package com.quest.security;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
+@Getter
+@Setter
 public class JwtUserDetails implements UserDetails {
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
+    private Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
 
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
 
     @Override
     public boolean isAccountNonExpired() {
