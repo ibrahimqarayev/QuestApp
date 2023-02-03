1e4cc6e647c145cfcab1bafcc2623cd503bb590d
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-03 23:38:15 +0400

    UserDto

---
+++ src/main/java/com/quest/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.quest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private String userName;
+}
