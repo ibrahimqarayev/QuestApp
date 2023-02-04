ff24d612292c01e220aaed0eaee0de11604bdc65
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-04 23:36:38 +0400

    UserDetailsServiceImpl

--- src/main/java/com/quest/service/UserDetailsServiceImpl.java
+++ src/main/java/com/quest/service/UserDetailsServiceImpl.java
@@ -1,4 +1,30 @@
 package com.quest.service;
 
-public class UserDetailsServiceImpl {
+import com.quest.entity.User;
+import com.quest.repository.UserRepository;
+import com.quest.security.JwtUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        return JwtUserDetails.create(user);
+    }
+
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).get();
+        return JwtUserDetails.create(user);
+    }
+
 }
