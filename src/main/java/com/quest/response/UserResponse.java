17a555ce32fda6adf743f85d20bbc081dd7bfe58
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-05 21:38:42 +0400

    UserResponse

--- src/main/java/com/quest/response/UserResponse.java
+++ src/main/java/com/quest/response/UserResponse.java
@@ -1,4 +1,18 @@
 package com.quest.response;
 
+import com.quest.entity.User;
+import lombok.Data;
+
+@Data
 public class UserResponse {
+
+    Long id;
+    int avatarId;
+    String userName;
+
+    public UserResponse(User entity) {
+        this.id = entity.getId();
+        this.avatarId = entity.getAvatar();
+        this.userName = entity.getUserName();
+    }
 }
