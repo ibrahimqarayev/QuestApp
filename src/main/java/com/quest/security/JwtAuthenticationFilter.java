a02d4bf9e7679e95d3b36bd28c591578c4d581a5
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-05 21:32:42 +0400

    JwtAuthenticationFilter

--- src/main/java/com/quest/security/JwtAuthenticationFilter.java
+++ src/main/java/com/quest/security/JwtAuthenticationFilter.java
@@ -1,7 +1,19 @@
 package com.quest.security;
 
+import com.quest.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
