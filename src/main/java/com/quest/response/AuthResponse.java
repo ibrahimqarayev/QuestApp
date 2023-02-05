ca5b89587320b24f16257c4ea2bc1d5b356ef543
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-05 21:40:14 +0400

    AuthResponse

--- src/main/java/com/quest/response/AuthResponse.java
+++ src/main/java/com/quest/response/AuthResponse.java
@@ -1,4 +1,12 @@
 package com.quest.response;
 
+import lombok.Data;
+
+@Data
 public class AuthResponse {
+
+    String message;
+    Long userId;
+    String accessToken;
+    String refreshToken;
 }
