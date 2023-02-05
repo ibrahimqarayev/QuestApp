6839e73f1df065191444ff97effd51fb2c92bab5
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-05 21:40:56 +0400

    JwtAuthenticationFilter

--- src/main/java/com/quest/security/JwtAuthenticationFilter.java
+++ src/main/java/com/quest/security/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.quest.security;
 
-import com.quest.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
