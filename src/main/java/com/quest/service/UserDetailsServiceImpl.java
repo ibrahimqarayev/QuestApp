fa57003dc580599fe91310f6a7219a86eb5d9190
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-05 21:31:01 +0400

    UserDetailsServiceImpl

--- src/main/java/com/quest/service/UserDetailsServiceImpl.java
+++ src/main/java/com/quest/service/UserDetailsServiceImpl.java
@@ -5,6 +5,7 @@ import com.quest.repository.UserRepository;
 import com.quest.security.JwtUserDetails;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
