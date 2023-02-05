7cfad75a99f9c10b1f810f53de8e2a98fc6bb03b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-05 21:39:22 +0400

    RefreshToken

--- src/main/java/com/quest/entity/RefreshToken.java
+++ src/main/java/com/quest/entity/RefreshToken.java
@@ -1,9 +1,13 @@
 package com.quest.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="refresh_token")
@@ -13,5 +17,18 @@ public class RefreshToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable=false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    User user;
+
+    @Column(nullable = false, unique = true)
+    String token;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    Date expiryDate;
 
 }
