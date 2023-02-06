abe9ee79fb9413d712f358b17de62cadb406c93e
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-02-06 23:46:15 +0400

    UserNotFoundException.java

--- src/main/java/com/quest/exception/UserNotFoundException.java
+++ src/main/java/com/quest/exception/UserNotFoundException.java
@@ -1,4 +1,12 @@
 package com.quest.exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException() {
+        super();
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
